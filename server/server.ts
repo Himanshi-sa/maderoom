ff6b02e5c34c7c9d1aba4ef1508dc05894a5fa90
Himanshi-sa <173894294+Himanshi-sa@users.noreply.github.com>
2025-02-09 08:13:51 +0000

    add io

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,26 @@
-console.log("Hello via Bun!");
+ import http from 'http';
+ import express from 'express';
+ import { Server } from 'socket.io';
+
+const app= express();
+const PORT = 8000;
+const server = http.createServer(app);
+
+//MARK: api route
+app.get('/',(req,res) => {
+    res.send("sockrt.io is heathy")
+})
+//MARK: socket.io
+const io = new Server(server,{
+    cors:{
+        origin:"*" //for cors
+    }       
+});
+
+io.on('connection',(socket) => {
+    console.log("some one is connected",socket)
+})
+
+server.listen(PORT,() => {
+    console.log("server is  up running on port 3000")
+})
